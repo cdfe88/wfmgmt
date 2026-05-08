c4d6727db7337a88d44d82b4001f5a619ba6b788
cdfe88 <62770548+cdfe88@users.noreply.github.com>
2026-05-08 15:15:38 +0000

    df

--- streamlit_app.py
+++ streamlit_app.py
@@ -555,6 +555,8 @@ if __name__ == '__main__':
             p_wf,p_sch,p_r_sch=calculate_resources(pdemand)
             hwft=pd.DataFrame(list(h_wf.values()), index=h_wf.keys(),columns=['Historical'])
             pwft=pd.DataFrame(list(p_wf.values()), index=p_wf.keys(),columns=['Projected'])
+            st.dataframe(hwft)
+            st.dataframe(pwft)
             twf=pd.merge(hwft,pwft)
             st.table(twf.reset_index().T)
             fig3,fig4,fig5=create_demand_plot(hdemand,pdemand)
